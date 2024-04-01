8062926fb0a6c8d4424d65a4c920445d640919be
Vadepeer04 <102405641+Vadepeer@users.noreply.github.com>
2024-04-01 16:24:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def download_file(filename):
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
